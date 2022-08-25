51e391b407e21b5885615404681c447fb347f154
JorgEdu1 <je33542@gmail.com>
2022-08-25 16:32:22 -0300

    soldados

---
+++ Atividades ED/Soldados/main.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+
+    int matriz[3][3];
+
+    for(int i = 0; i < 3; i++){
+
+        for(int j = 0; j < 3; j++){
+
+             cin >> matriz[i][j];
+
+        }
+
+    }
+
+    int cont = 0;
+
+    for(int i = 0; i < 2; i++){
+
+        for(int j = 0; j < 3; j++){
+
+            if(matriz[i][j] > matriz[i+1][j]){
+
+                cont++;
+
+            }
+
+        }
+
+    }
+
+    cout << cont;
+
+    return 0;
+}
