afa1f58c5f25226bf6aa35141711e6ec90e907b7
jorgedu1 <je33542@gmail.com>
2022-08-26 08:46:03 -0300

    gansos

---
+++ Atividades ED/Gansos_Gamados/main.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+//nao terminada.
+
+int main(){
+
+    string vetor;
+    getline(std::cin, vetor);
+    int tam = vetor.size();
+
+    bool check = false;
+
+    for(int i=0; i< tam-1; i++){
+
+        
+
+    }
+
+    if(check == true){
+
+        cout << "sim";
+
+    }else {
+
+        cout << "nao";
+
+    }
+    
+    return 0;
+}
